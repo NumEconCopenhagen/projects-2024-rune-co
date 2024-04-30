55f0325d5980388c8bab04999d022021cfa63cf4
hfz196 <hfz196@alumni.ku.dk>
2024-04-30 17:22:19 +0200

    model project

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,11 +1,23 @@
-# endowments
-e1 = 24
-e2 = 0
-
 # utility function
-def u(x1,x2,alpha=1/2):
+def u(x1,x2,alpha):
     return x1**alpha*x2**(1-alpha)
 
 # production function
-def f(z,beta=1/2):
+def y(z,beta):
     return z**beta
+
+#income
+def income(x1,p1,e1,e2,beta):
+    return x1*p1+e2+(e1-x1)**beta
+
+# demand functions
+def demand1(alpha,I,p1):
+    return alpha*I/p1
+def demand2(alpha,I):
+    return (1-alpha)*I
+
+#excess demand
+def excessdemand1(e1,x1):
+    return sum(e1) - sum(x1)
+def excessdemand2(e2,x2):
+    return sum(e2) - sum(x2)
