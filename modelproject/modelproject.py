82f105dfd1ee28886a49c06ba39b1e5a35f2d77d
hfz196 <hfz196@alumni.ku.dk>
2024-05-08 18:36:49 +0200

    finished model project!

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -4,6 +4,10 @@ import numpy as np
 def u(x1,x2,alpha):
     return x1**alpha*x2**(1-alpha)
 
+# utility function in robinson crusoe economy
+def u_koop(x1,x2,z,alpha,beta):
+    return (x1 - z)**alpha * (x2 + z**beta)**(1-alpha)
+
 # production function
 def y(z,beta):
     return z**beta
@@ -23,40 +27,3 @@ def excessdemand1(e1,x1):
     return sum(e1) - sum(x1)
 def excessdemand2(e2,x2):
     return sum(e2) - sum(x2)
-
-class IslandEconomy:
-    def __init__(self, N):
-        self.N = N
-        self.alphas = np.random.random(N)
-        self.betas = np.random.random(N)
-        self.e1s = np.random.random(N)
-        self.e2s = np.random.random(N)
-    
-    # Utility function
-    def utility(x1,x2,alpha):
-        return x1**alpha * x2**(1-alpha)
-    
-    # Production function 
-    def production(z,beta):
-        return z**beta
-    
-    # Income function
-    def income(x1_pre,p1,e1,e2,beta):
-        z = e1 - x1_pre
-        x2_pre = e2 + production(z,beta)
-        return p1*x1_pre+x2_pre
-    
-    # Demand functions
-    def demand1(p1,alpha,I):
-        return alpha*I/p1
-    def demand2(alpha,I):
-        return (1-alpha)*I
-    
-    # Excess demand functions
-    def excessdemand1(e1,x1):
-        return sum(e1) - sum(x1)
-    def excessdemand2(e2,x2):
-        return sum(e2) - sum(x2)
-    
-
-
